9a4b0921d6322dec3c4340dd8aa66c30f742d6d3
kimson14569 <anhyeucay1987>
2019-05-17 20:02:51 +0700

    qwe

---
+++ index.js
@@ -0,0 +1,29 @@
+let pg = require('pg')
+let client = new pg.Client(
+    {
+        user: 'postgres',
+        password: 'postgres',
+        database: 'csv',
+        host: 'localhost',
+        port: 5432
+    }
+)
+
+client.connect()
+
+//client.query('select * from dm_nv', function(err, result) {
+//    console.log(err, result.rows)
+//})
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    client.query('select * from dm_nv', function(err, result) {
+    res.send(JSON.stringify(result.rows))
+    client.end()
+})
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
